3589632103e0cdc5822c092c48acddd2cd54f17c
Samarth Upadhyaya <samarthupadhyaya@Samarths-MacBook-Pro.local>
2021-07-23 09:54:10 +0800

    Created skeleton of article search page

---
+++ components/NavigationBar.js
@@ -0,0 +1,20 @@
+import styles from '../styles/NavigationBar.module.css'
+import { useRouter } from 'next/router'
+import MailIcon from '@material-ui/icons/Mail'
+
+const NavigationBar = () => {
+	const router = useRouter()
+	return (
+		<div
+			className={styles.navigationBar}
+			onClick={() => router.push('/')}
+		>
+			<a>Article Assimilator</a>
+			<div className={styles.navBarSpace}></div>
+			<a href="mailto:samarthupadhyaya27@gmail.com">Contact</a>
+			<MailIcon/>
+		</div>
+	)
+}
+
+export default NavigationBar
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { Fragment } from 'react'
+import NavigationBar from '../components/NavigationBar'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }) {
+  return (
+		<Fragment>
+			<NavigationBar />
+			<Component {...pageProps} />
+		</Fragment>
+  )
 }
 
 export default MyApp
